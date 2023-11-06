bdc96e899d93f5e48da1008bef84c538f65076e1
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-07 01:04:13 +0530

    Uc5 added

--- SnakeAndLadder/SnakeAndLadder.java
+++ SnakeAndLadder/SnakeAndLadder.java
@@ -5,12 +5,7 @@ import java.util.Scanner;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int playerPosition = 0;
-        if (playerPosition < 0)
-        {
-            playerPosition = 0;
-        }
-
-        System.out.println("Player's current position: " + playerPosition);
+     
+            System.out.println("Congratulations! Player reached the winning spot.");
     }
     }
