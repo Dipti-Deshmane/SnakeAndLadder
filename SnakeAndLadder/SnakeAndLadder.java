8e4e6e71a8701ef799f89352b0c07322d4c3ebed
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-07 00:28:26 +0530

    Added snakeAndLadder

---
+++ SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package SnakeAndLadder;
+
+import java.util.Random;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        int winningPosition = 100;
+        System.out.println("Snake and Ladder Game Simulation");
+
+    }
+}
