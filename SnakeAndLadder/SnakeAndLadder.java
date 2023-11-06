53d509cba4e05eb93927410e6c67ed97aafc889c
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-07 01:02:54 +0530

    Uc4 added

--- SnakeAndLadder/SnakeAndLadder.java
+++ SnakeAndLadder/SnakeAndLadder.java
@@ -6,22 +6,11 @@ import java.util.Scanner;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int playerPosition = 0;
-        Random random = new Random();
-        int diceRoll = random.nextInt(6) + 1;
-
-        int option = random.nextInt(3);
-        switch (option) {
-            case 0:
-                System.out.println("Player rolled a " + diceRoll + ". Stay at same position.");
-                break;
-            case 1:
-                System.out.println("Player rolled a " + diceRoll + ". It's a ladder! Moving up.");
-                playerPosition += diceRoll;
-                break;
-            case 2:
-                System.out.println("Player rolled a " + diceRoll + ". it's a snake! Moving down.");
-                playerPosition -= diceRoll;
-                break;
+        if (playerPosition < 0)
+        {
+            playerPosition = 0;
         }
+
+        System.out.println("Player's current position: " + playerPosition);
     }
-}
+    }
