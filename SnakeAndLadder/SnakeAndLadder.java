7755b904dec36181b70586cf08492f42d452496c
Dipti-Deshmane <itsmedipu17@gmail.com>
2023-11-07 00:40:59 +0530

    Uc2 added

--- SnakeAndLadder/SnakeAndLadder.java
+++ SnakeAndLadder/SnakeAndLadder.java
@@ -1,12 +1,15 @@
 package SnakeAndLadder;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int playerPosition = 0;
-        int winningPosition = 100;
-        System.out.println("Snake and Ladder Game Simulation");
+
+        Scanner random = null;
+        int diceRoll = random.nextInt(6) + 1;
+
+        
 
     }
 }
